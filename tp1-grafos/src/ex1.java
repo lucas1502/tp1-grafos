befd40d7e369c9f2496fcb596d64279c82477ad3
EderOD <37964092+EderOD@users.noreply.github.com>
2018-09-28 14:10:46 -0300

    Manipulação de String
    Foi adicionado modelo para leitura de string utilizando File.
    Para isso foi necessário a utilização de 5 bibliotecas.

--- tp1-grafos/src/ex1.java
+++ tp1-grafos/src/ex1.java
@@ -1,8 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class ex1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Olá, git!");
+		String path = "file.txt";
+		BufferedReader buffRead = new BufferedReader(new FileReader(path));
+		String linha = "";
+		while (linha != null) {
+			System.out.println(linha);
+			linha = buffRead.readLine();
+		}
+		buffRead.close();
 		//Commit gerado pelo git
 		//Commit gerado pelo eclipse
 		//Commit feito por outra conta
