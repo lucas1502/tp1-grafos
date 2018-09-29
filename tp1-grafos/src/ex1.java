12a4d742cf5cece3631f43dd0853f30336c39a78
EDER <EDER@DESKTOP-DTFK6KA>
2018-09-28 23:17:25 -0300

    Correção da manipulação de arquivo

--- tp1-grafos/src/ex1.java
+++ tp1-grafos/src/ex1.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -10,13 +8,22 @@ public class ex1 {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Olá, git!");
 		String path = "file.txt";
-		BufferedReader buffRead = new BufferedReader(new FileReader(path));
-		String linha = "";
-		while (linha != null) {
-			System.out.println(linha);
-			linha = buffRead.readLine();
+		int u,v,peso,direc;
+		char c;
+		String aux;
+		try {
+			BufferedReader buffRead = new BufferedReader(new FileReader(path));
+			String linha = "";
+			while (linha != null) {
+				System.out.println(linha);
+				linha = buffRead.readLine();
+			}
+			buffRead.close();
+		} catch (IOException e) {
+			System.err.printf("Erro na abertura do arquivo: %s. \n", e.getMessage());
 		}
-		buffRead.close();
+		
+		
 	}
 
 }
