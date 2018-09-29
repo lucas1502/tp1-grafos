cc648f543df2525fbdc9203c0c94575a09f31b18
EderOD <37964092+EderOD@users.noreply.github.com>
2018-09-29 13:35:57 -0300

    Implementação na leitura de grafos dirigidos e não dirigidos

--- tp1-grafos/src/ex1.java
+++ tp1-grafos/src/ex1.java
@@ -8,17 +8,20 @@ public class ex1 {
 	public static void main(String[] args) throws IOException {
 		System.out.println("OlÃ¡, git!");
 		String path = "file.txt";
-		int inteiros[] = new int[4];
+		int vertices = 0;					// numero de vertices no grafo
+		int inteiros[] = new int[4];		// vetor = [verticeU, verticeV, peso, direção]
 		String aux = "";
 		try {
-			BufferedReader buffRead = new BufferedReader(new FileReader(path));
+			BufferedReader buffRead = new BufferedReader(new FileReader(path)); 
 			String linha = "";
 			int i = 0;
-			
-			while ((linha = buffRead.readLine()) != null) {
+			linha = buffRead.readLine();		// Primeira linha é lida
+			vertices = Integer.parseInt(linha); // Primeiro valor é do numero de vertices, converte para inteiro
+			while ((linha = buffRead.readLine()) != null) {		// ler todas as linhas do arquivo
+				aux = "";
 				i = 0;
 				System.out.println(linha);	
-				for(char c : linha.toCharArray()) {
+				for(char c : linha.toCharArray()) {		// A linha é dividida char a char para guardar no vetor
 					if(c == ';') {
 						inteiros[i++] = Integer.parseInt(aux);
 						aux = "";
@@ -26,9 +29,11 @@ public class ex1 {
 					} else if (c == ' ') {
 						continue;
 					} else {
-						aux = aux + c;
+						aux = aux + c;		// Concatena os char na string porque pode ser um numero mais de 1 digito
 					}
 				}
+				inteiros[i] = Integer.parseInt(aux);
+				
 				
 			}
 			buffRead.close();
