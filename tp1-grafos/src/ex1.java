d2288b70b9c58d4ef562d9ea1303d0445c9e7e67
Lucas <Lucas@192.168.15.8>
2018-09-30 11:18:02 -0300

    addicionei uma linha de codigo Carfvalho

--- tp1-grafos/src/ex1.java
+++ tp1-grafos/src/ex1.java
@@ -3,9 +3,9 @@ import java.io.IOException;
 public class ex1 {
 
 	public static void main(String[] args) throws IOException {
-		Grafo g = ManipuladorFile.ler("file.txt");
+		Grafo g = ManipuladorFile.ler("C:\\Users\\Lucas\\git\\tp1-grafos\\tp1-grafos\\file.txt");
 		System.out.println("Hello mundu!");
-		
+		//  Commit Carvalho
 		
 		
 	}
