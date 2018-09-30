ceb0c0c1ee19b3e3d59c1398d892ba8ab58c3b27
EderOD <37964092+EderOD@users.noreply.github.com>
2018-09-30 15:40:35 -0300

    adicionado funções na classe Nao dirigido

--- tp1-grafos/src/NaoDirigido.java
+++ tp1-grafos/src/NaoDirigido.java
@@ -8,4 +8,24 @@ public class NaoDirigido extends Grafo{
 		this.setVertices(v1, v2, peso);
 		this.setVertices(v2, v1, peso);
 	}
+	public int getGrauEntrada(int v1) {
+	 int grau=0;
+	for(int j =0;j<this.getTam(); j++) {
+		if(this.arestas[v1][j]!=null) {
+			grau=grau+arestas[v1][j].size();
+		}
+	}
+	return grau;
+		
+	}
+	
+	public int getGrauSaida(int v1) {
+		int grau=0;
+		for(int j =0;j<this.getTam(); j++) {
+			if(this.arestas[j][v1]!=null) {
+				grau=grau+arestas[j][v1].size();
+			}
+		}
+		return grau;
+	}
 }
