aaffb65a94895024847aec758a6e71a159c064ea
Lucas <Lucas@192.168.15.8>
2018-09-30 11:23:21 -0300

    comittttt3

--- tp1-grafos/src/ex1.java
+++ tp1-grafos/src/ex1.java
@@ -5,7 +5,7 @@ public class ex1 {
 	public static void main(String[] args) throws IOException {
 		Grafo g = ManipuladorFile.ler("C:\\Users\\Lucas\\git\\tp1-grafos\\tp1-grafos\\file.txt");
 		System.out.println("Hello mundu!");
-		//  Commit Carvalho2
+		//  Commit Carvalho3
 		
 		
 	}
